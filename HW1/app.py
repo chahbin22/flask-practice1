199a9bdb05bbd01f13fe12a69a1f256e3d567503
형빈 <22011691@sju.ac.kr>
2026-09-22 12:11:01 +0900

    HW1: 세팅&app.py 작성

---
+++ HW1/app.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def hi_template_render():
+    return render_template("hb.html")
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+@app.route("/greet/<name>")
+def greet(name):
+    return render_template("greet.html", name=name) 
+
+if __name__ == "__main__":
+    app.run(debug=True)
